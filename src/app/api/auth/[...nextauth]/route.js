7edcad825890e41fad5171677e417edbacaeaea1
Mohammad Ragib <mdragib.mth2005@gmail.com>
2026-06-09 00:14:38 +0530

    google auth completed

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,40 @@
+// import NextAuth from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+
+// const handler = NextAuth({
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     }),
+//   ],
+// });
+
+// export { handler as GET, handler as POST };
+
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import AppleProvider from "next-auth/providers/apple";
 
 const handler = NextAuth({
+  
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
+
+    AppleProvider({
+      clientId: process.env.APPLE_ID,
+      clientSecret: process.env.APPLE_SECRET,
+    }),
   ],
+
+  secret: process.env.NEXTAUTH_SECRET,
 });
+
+console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
+console.log("GOOGLE_CLIENT_SECRET length:", process.env.GOOGLE_CLIENT_SECRET?.length);
+console.log("GOOGLE_CLIENT_SECRET tail:", process.env.GOOGLE_CLIENT_SECRET?.slice(-4));
 
 export { handler as GET, handler as POST };
